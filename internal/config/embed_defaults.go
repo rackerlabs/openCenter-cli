dbc2b7aae6605d1a30ed1047bba2035243556cdd
Victor Palma <palma.victor@gmail.com>
2026-01-09 10:51:50 -0600

    Remove dead code: unused defaultMainTF variable
    - Removed unused defaultMainTF variable and its embed directive
    - The defaults/main.tf file never existed and the variable was never used
    - Current system uses template-based approach (main.tf.v0.tpl) which is more flexible
    - Restored openstack.yaml file that was accidentally deleted
    - Cleaned up embed_defaults.go to only contain actively used embedded content

--- internal/config/embed_defaults.go
+++ internal/config/embed_defaults.go
@@ -2,11 +2,6 @@ package config
 
 import _ "embed"
 
-// Default content for iac.main_tf seeded from defaults.
-//
-//go:embed defaults/main.tf
-var defaultMainTF string
-
 // Default structured IaC values (locals as main, and modules) parsed from Terraform
 // and expressed as YAML. Used to seed iac.main and iac.modules during cluster init.
 //
